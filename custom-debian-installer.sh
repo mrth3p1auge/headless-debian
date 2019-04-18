43058ed244fdef0dc77fb5d21612d369687a9bba
cscutt <chris.scutt@gmail.com>
2019-04-18 08:08:45 -0400

    installer intial commit

---
+++ custom-debian-installer.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# Install prerequisites
+#
+# You should be running the same flavor as the installer you are producing.
+# Example, an installer for Debian Stretch can only produced on a Strech machine.
+
+sudo apt-get update
+sudo apt-get install -y simple-cdd git xorriso
+
+# Remove files from previous run 
+rm -fr tmp; 
+rm -fr images; 
+
+# Build installer
+build-simple-cdd -s --conf profiles/headless.conf --dist stretch
+
+# Installer is in Images folder
